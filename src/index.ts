d6d31f0c82d94f2c58610c49551fce1f39cf9472
Jon <rccommandotac@gmail.com>
2022-05-02 14:56:33 -0500

    string and dataview hashing boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,53 @@
 
-export default class MyPlugin {
-  constructor () {
-    console.log("Hello World");
+/** 
+  * Peer
+  * - id list, hash
+  */
+
+const cyrb53 = {
+  /**
+   * https://stackoverflow.com/a/52171480/8112809
+   * @param str 
+   * @param seed 
+   * @returns 53 bit hash of string
+   */
+  string(str: string, seed: number = 0): number {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    let ch: number;
+    for (let i = 0; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+  },
+  /**
+   * Modified from https://stackoverflow.com/a/52171480/8112809 to support dataview hashing
+   * @param view 
+   * @param seed 
+   * @returns 
+   */
+  dataview (view: DataView, seed: number = 0): number {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    let ch: number;
+    let length = view.byteLength;
+    for (let i = 0; i < length; i++) {
+      ch = view.getUint8(i); //str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
   }
+};
+
+export async function test() {
+
+
+
 }
 
-let myPluginInst = new MyPlugin();
+test();
